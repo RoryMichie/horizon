e42c57097d92a77b129c279d154faaef9187c405
Andrew Gillespie <25agillespie@gmail.com>
2022-11-27 13:43:36 -0500

    format the example scripts

--- scripts/gtn.lua
+++ scripts/gtn.lua
@@ -1,17 +1,17 @@
 require "io"
 require "math"
-target=math.random(1,100)
+target = math.random(1, 100)
 io.write("Welcome to guess the number. Let's see if you can find my number, between 1 and 100")
 while true do
-	io.write("Enter your guess\n")
-	g=tonumber(io.read())
-	if g<target then
-	io.write("Too low!\n")
-	elseif g>target then
-		io.write("Too high!\n")
-	else
-		io.write("Yay, you win!\n")
-		wait(1)
-		break
-	end
+    io.write("Enter your guess\n")
+    g = tonumber(io.read())
+    if g < target then
+        io.write("Too low!\n")
+    elseif g > target then
+        io.write("Too high!\n")
+    else
+        io.write("Yay, you win!\n")
+        wait(1)
+        break
+    end
 end
--- scripts/keyboard.lua
+++ scripts/keyboard.lua
@@ -1,4 +1,3 @@
-
 chaos.Initialize("keyboard test")
 speak("initialised")
 chaos.TrackKey("a")
@@ -6,16 +5,16 @@ chaos.TrackKey("space")
 chaos.TrackKey("escape")
 speak("tracked keys")
 while true do
-	chaos.GetCurrentFrameEvents()
-	if chaos.KeyPressed("space")==1 then
-		speak("beep")
-	end
-	if chaos.KeyPressed("a")==1 then
-		speak("A is for apple.")
-	end
-	if chaos.KeyPressed("escape")==1 then
-		speak("Goodbye")
-		chaos.shutdown()
-		break
-	end
+    chaos.GetCurrentFrameEvents()
+    if chaos.KeyPressed("space") == 1 then
+        speak("beep")
+    end
+    if chaos.KeyPressed("a") == 1 then
+        speak("A is for apple.")
+    end
+    if chaos.KeyPressed("escape") == 1 then
+        speak("Goodbye")
+        chaos.shutdown()
+        break
+    end
 end
